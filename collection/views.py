255bcf2e4a9ae6fa128e6d33927058d19a29ef3e
[] <awh938@gmail.com>
2019-10-17 13:50:26 -0400

    created static views w/templates

--- collection/views.py
+++ collection/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    # this is your new views
-    return render(request, 'index.html')
+    # defining the variable
+    number = 6
+    # defining string variable
+    thing = "Lucy Lou"
+    # passing the variable to the views
+    return render(request, 'index.html', {
+        'number': number,
+        'thing' : thing,
+    })
