fc6fa3e19262aa59dcdf848b75a3c0b63ae19799
[] <awh938@gmail.com>
2019-10-17 15:23:04 -0400

    created database migration & admin user

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+# import your model
+from collection.models import Word
+
+# setup automated slug creation
+class WordAdmin(admin.ModelAdmin):
+    model = Word
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug' :('name',)}
+
 # Register your models here.
+admin.site.register(Word, WordAdmin)
